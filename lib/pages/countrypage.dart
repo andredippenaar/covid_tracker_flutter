82e25deb8ebe5ad940610d2c5dac09768357e07c
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2020-04-14 11:40:57 +0200

    Implemented Search

--- lib/pages/countrypage.dart
+++ lib/pages/countrypage.dart
@@ -1,3 +1,4 @@
+import 'package:covidtrackerflutter/pages/search.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -33,6 +34,17 @@ class _CountryPageState extends State<CountryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: Search(countryData),
+              );
+            },
+          )
+        ],
         title: Text('Country Stats'),
       ),
       body: countryData == null
@@ -42,90 +54,92 @@ class _CountryPageState extends State<CountryPage> {
             )
           : ListView.builder(
               itemBuilder: (context, index) {
-                return Container(
-                  height: 130.0,
-                  margin:
-                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey[200],
-                            blurRadius: 10.0,
-                            offset: Offset(0, 10))
-                      ]),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              countryData[index]['country'],
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Image.network(
-                              countryData[index]['countryInfo']['flag'],
-                              height: 100.0,
-                              width: 130.0,
-                            )
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          margin: EdgeInsets.only(left: 40.0),
+                return Card(
+                  child: Container(
+                    height: 130.0,
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey[200],
+                              blurRadius: 10.0,
+                              offset: Offset(0, 10))
+                        ]),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 10.0),
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
                               Text(
-                                'CONFIRMED: ' +
-                                    formatter
-                                        .format(countryData[index]['cases']),
-                                style: TextStyle(
-                                    color: Colors.red,
-                                    fontWeight: FontWeight.bold),
+                                countryData[index]['country'],
+                                style: TextStyle(fontWeight: FontWeight.bold),
                               ),
-                              Text(
-                                "[+${countryData[index]['todayCases']}]",
-                                style: TextStyle(color: Colors.red),
-                              ),
-                              Text(
-                                'ACTIVE: ' +
-                                    formatter
-                                        .format(countryData[index]['active']),
-                                style: TextStyle(
-                                  color: Colors.blue,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                'RECOVERED: ' +
-                                    formatter.format(
-                                        countryData[index]['recovered']),
-                                style: TextStyle(
-                                    color: Colors.green,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                'DEATHS: ' +
-                                    formatter
-                                        .format(countryData[index]['deaths']),
-                                style: TextStyle(
-                                    color: Colors.grey[800],
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                "[+${countryData[index]['todayDeaths']}]",
-                                style: TextStyle(color: Colors.grey[800]),
-                              ),
+                              Image.network(
+                                countryData[index]['countryInfo']['flag'],
+                                height: 100.0,
+                                width: 130.0,
+                              )
                             ],
                           ),
                         ),
-                      )
-                    ],
+                        Expanded(
+                          child: Container(
+                            margin: EdgeInsets.only(left: 40.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'CONFIRMED: ' +
+                                      formatter
+                                          .format(countryData[index]['cases']),
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "[+${countryData[index]['todayCases']}]",
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                                Text(
+                                  'ACTIVE: ' +
+                                      formatter
+                                          .format(countryData[index]['active']),
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'RECOVERED: ' +
+                                      formatter.format(
+                                          countryData[index]['recovered']),
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'DEATHS: ' +
+                                      formatter
+                                          .format(countryData[index]['deaths']),
+                                  style: TextStyle(
+                                      color: Colors.grey[800],
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "[+${countryData[index]['todayDeaths']}]",
+                                  style: TextStyle(color: Colors.grey[800]),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 );
               },
---
+++ lib/pages/search.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = new NumberFormat("#,###");
+
+class Search extends SearchDelegate {
+  final List countryList;
+
+  Search(this.countryList);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back_ios),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? countryList
+        : countryList
+            .where(
+              (element) =>
+                  element['country'].toString().toLowerCase().startsWith(query),
+            )
+            .toList();
+    return ListView.builder(
+        itemCount: suggestionList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Container(
+              height: 130.0,
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              decoration: BoxDecoration(color: Colors.grey[300], boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[200],
+                    blurRadius: 10.0,
+                    offset: Offset(0, 10))
+              ]),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          suggestionList[index]['country'],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Image.network(
+                          suggestionList[index]['countryInfo']['flag'],
+                          height: 100.0,
+                          width: 130.0,
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 40.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'CONFIRMED: ' +
+                                formatter
+                                    .format(suggestionList[index]['cases']),
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "[+${suggestionList[index]['todayCases']}]",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                          Text(
+                            'ACTIVE: ' +
+                                formatter
+                                    .format(suggestionList[index]['active']),
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'RECOVERED: ' +
+                                formatter
+                                    .format(suggestionList[index]['recovered']),
+                            style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'DEATHS: ' +
+                                formatter
+                                    .format(suggestionList[index]['deaths']),
+                            style: TextStyle(
+                                color: Colors.grey[800],
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "[+${suggestionList[index]['todayDeaths']}]",
+                            style: TextStyle(color: Colors.grey[800]),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
