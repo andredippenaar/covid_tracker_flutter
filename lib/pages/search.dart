83442e139d2bb5bd2e50ceac2e655e213c693b48
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2020-04-14 14:22:26 +0200

    Updated App Icons, and App Display Name

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -31,7 +31,102 @@ class Search extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Container();
+    final suggestionList = query.isEmpty
+        ? countryList
+        : countryList
+            .where(
+              (element) =>
+                  element['country'].toString().toLowerCase().startsWith(query),
+            )
+            .toList();
+    return ListView.builder(
+        itemCount: suggestionList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Container(
+              height: 130.0,
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              decoration: BoxDecoration(color: Colors.grey[300], boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[200],
+                    blurRadius: 10.0,
+                    offset: Offset(0, 10))
+              ]),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          suggestionList[index]['country'],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Image.network(
+                          suggestionList[index]['countryInfo']['flag'],
+                          height: 100.0,
+                          width: 130.0,
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 40.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'CONFIRMED: ' +
+                                formatter
+                                    .format(suggestionList[index]['cases']),
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "[+${suggestionList[index]['todayCases']}]",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                          Text(
+                            'ACTIVE: ' +
+                                formatter
+                                    .format(suggestionList[index]['active']),
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'RECOVERED: ' +
+                                formatter
+                                    .format(suggestionList[index]['recovered']),
+                            style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'DEATHS: ' +
+                                formatter
+                                    .format(suggestionList[index]['deaths']),
+                            style: TextStyle(
+                                color: Colors.grey[800],
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "[+${suggestionList[index]['todayDeaths']}]",
+                            style: TextStyle(color: Colors.grey[800]),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   @override
